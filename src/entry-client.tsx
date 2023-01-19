92c954318f642b1831991d2ceaa79263709a5796
Riley Bruins <ribru17@hotmail.com>
2023-01-18 22:35:01 -0800

    temporarily giving up on analytics, waiting for bug fix

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,8 +1,3 @@
 import { mount, StartClient } from "solid-start/entry-client";
-// import { inject } from "@vercel/analytics/";
 
 mount(() => <StartClient />, document);
-// mount(() => {
-//     inject()
-//     return <StartClient />
-// }, document)
