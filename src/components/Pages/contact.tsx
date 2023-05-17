03be4f4dfb70a398107fd2e16328865b72b7b700
Riley Bruins <ribru17@hotmail.com>
2023-05-16 18:49:18 -0700

    fix: add the actual files like a smart person

---
+++ src/components/Pages/contact.tsx
@@ -0,0 +1,115 @@
+import { createSignal, JSX, onCleanup, onMount } from 'solid-js';
+import Github from '~/components/SVGs/Github/Github';
+import LinkedIn from '~/components/SVGs/LinkedIn/LinkedIn';
+import CoolButton from '~/components/CoolButton/CoolButton';
+import './contact.css';
+
+export default function Contact() {
+  interface ContactForm extends HTMLFormControlsCollection {
+    name?: HTMLInputElement;
+    message?: HTMLInputElement;
+    submit?: HTMLInputElement;
+  }
+
+  let formRef: HTMLFormElement | undefined = undefined;
+
+  const [buttonText, setButtonText] = createSignal('Send');
+  const [active, setActive] = createSignal(false);
+
+  let mainRef: HTMLElement | undefined = undefined;
+
+  function handleScroll() {
+    let windowHeight = window.innerHeight;
+
+    let elementTop = mainRef?.getBoundingClientRect().top;
+
+    let elementVisible = window.innerHeight / 8;
+
+    if (elementTop && elementTop < windowHeight - elementVisible) {
+      setActive(true);
+    }
+  }
+
+  onMount(() => {
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+  });
+
+  onCleanup(() => {
+    window.removeEventListener('scroll', handleScroll);
+  });
+
+  const sendMail: JSX.EventHandlerUnion<
+    HTMLFormElement,
+    Event & { submitter: HTMLElement }
+  > = async (e) => {
+    e.preventDefault();
+    let fields: ContactForm = (e.target as HTMLFormElement).elements;
+    let rootUrl = 'https://portfolio-backend-actix.onrender.com';
+    let messageName = fields.name?.value || '';
+    let messageContent = fields.message?.value || '';
+    if (fields.name) {
+      fields.name.value = '';
+    }
+    if (fields.message) {
+      fields.message.value = '';
+    }
+    setButtonText('Message sent!');
+    setTimeout(() => {
+      setButtonText('Send');
+    }, 1000);
+    await fetch(`${rootUrl}/api/contact`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: messageName,
+        message: messageContent,
+      }),
+    });
+  };
+
+  return (
+    <main id='contact' class={active() ? 'active' : ''} ref={mainRef}>
+      <h1>Contact</h1>
+      <div id='contactInvite'>
+        <div id='logoCont'>
+          <a target='_blank' rel='noreferrer' href='https://github.com/ribru17'>
+            <Github />
+          </a>
+          <a
+            target='_blank'
+            rel='noreferrer'
+            href='https://www.linkedin.com/in/riley-bruins/'
+          >
+            <LinkedIn />
+          </a>
+        </div>
+        <p>
+          Check out my linked profiles on the left. If you have a question or
+          you want to work with me, send me an email below!
+        </p>
+      </div>
+      <form ref={formRef} onSubmit={sendMail} id='contactForm'>
+        <input
+          autocomplete='off'
+          type='text'
+          placeholder='Your email'
+          name='name'
+          required
+        >
+        </input>
+        <textarea
+          rows={3}
+          autocomplete='off'
+          placeholder='Your message'
+          name='message'
+          required
+        >
+        </textarea>
+        <CoolButton title={buttonText()} onClick={(_e) => formRef?.submit} />
+      </form>
+    </main>
+  );
+}
---
+++ src/components/Pages/experience.tsx
@@ -0,0 +1,131 @@
+import { createSignal, onCleanup, onMount } from 'solid-js';
+import './experience.css';
+
+export default function Experience() {
+  const [active, setActive] = createSignal(false);
+
+  let mainRef: HTMLElement | undefined = undefined;
+
+  function handleScroll() {
+    let windowHeight = window.innerHeight;
+
+    let elementTop = mainRef?.getBoundingClientRect().top;
+
+    let elementVisible = window.innerHeight / 8;
+
+    if (elementTop && elementTop < windowHeight - elementVisible) {
+      setActive(true);
+    }
+  }
+
+  onMount(() => {
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+  });
+
+  onCleanup(() => {
+    window.removeEventListener('scroll', handleScroll);
+  });
+
+  return (
+    <main id='experience' class={active() ? 'active' : ''} ref={mainRef}>
+      <h1>Experience</h1>
+      <div class='largeBlock' id='experienceBody'>
+        <h2 style={{ 'margin-top': 0 }}>Employment Experience</h2>
+        <div id='employmentExperienceCont'>
+          <div class='workCont'>
+            <div class='workInfoCont'>
+              <h3>SWE Intern at Google (incoming)</h3>
+              <h3 class='timeWorked'>June 2023 - September 2023</h3>
+            </div>
+            <p class='workText'>
+              Working in Mountain View, CA.
+            </p>
+          </div>
+          <div class='workCont'>
+            <div class='workInfoCont'>
+              <h3>AI Model Trainer at Surge AI</h3>
+              <h3 class='timeWorked'>June 2022 - August 2022</h3>
+            </div>
+            <p class='workText'>
+              Here I worked part-time with a team of over 80 people to write
+              helpful and production-quality code in order to teach an AI model
+              to do the same. My main focus was in Javascript and Typescript but
+              I also trained the bot in Rust and C.
+            </p>
+          </div>
+        </div>
+        <h2>Personal Experience</h2>
+        <div class='flex'>
+          <div class='workCont spaceBottom revOnMobile'>
+            <div id='experienceTextCont'>
+              <p id='experienceText'>
+                I mostly use Express with Node for my backends and favor React
+                as my frontend framework. This website, however, is built with
+                SolidJS and Typescript with a Rust (Actix Web) backend! Feel
+                free to{' '}
+                <a
+                  class='inlineLink'
+                  target='_blank'
+                  rel='noreferrer'
+                  href='https://github.com/ribru17/portfolio-site-solid'
+                >
+                  view this site's source code
+                </a>{' '}
+                or take a look at some of my skills below!
+              </p>
+            </div>
+            <div class='typewriterCont'>
+              <p class='typewriterStatic'>background-color:&nbsp;</p>
+              <p class='typewriter'></p>
+            </div>
+          </div>
+        </div>
+        <div id='experienceCont'>
+          <div class='bar'>
+            <div class='max'>Javascript / Typescript</div>
+          </div>
+          <div class='bar'>
+            <div class='max'>React.js / React Native</div>
+          </div>
+          <div class='bar'>
+            <div class='max'>Node.js</div>
+          </div>
+          <div class='bar'>
+            <div class='max'>HTML + CSS</div>
+          </div>
+          <div class='bar'>
+            <div class='max'>MongoDB</div>
+          </div>
+          <div class='bar'>
+            <div class='max'>Git</div>
+          </div>
+          <div class='bar'>
+            <div class='max'>Python</div>
+          </div>
+          <div class='bar'>
+            <div class='max'>C / C++</div>
+          </div>
+          <div class='bar'>
+            <div class='eighty'>Rust</div>
+          </div>
+          <div class='bar'>
+            <div class='eighty'>Lua</div>
+          </div>
+          <div class='bar'>
+            <div class='sixty'>Haskell</div>
+          </div>
+          <div class='bar'>
+            <div class='sixty'>C#</div>
+          </div>
+          <div class='bar'>
+            <div class='sixty'>Go</div>
+          </div>
+          <div class='bar'>
+            <div class='sixty'>R</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
